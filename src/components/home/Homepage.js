e5bb71a3603f5e0afb901d42ce9aa797718be76d
Sakhumzi kwaza <sakhumzikwaza220@gmail.com>
2024-03-05 07:13:49 +0200

    homepage added

--- src/components/home/Homepage.js
+++ src/components/home/Homepage.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./homepage.css";
 
-function homepage(){
-    return{
-        
-    }
+function Homepage(){
+    return(
+     <div className="homepage">
+        <h1>Home</h1>
+     </div>
+    )
 }
+export default Homepage
