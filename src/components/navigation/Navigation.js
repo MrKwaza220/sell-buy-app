ad90183b924982316b97508400780af56f6e97a1
Sakhumzi kwaza <sakhumzikwaza220@gmail.com>
2024-03-05 07:12:57 +0200

    Navigation added

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,2 +1,32 @@
 import React from "react";
-import "./Nav"
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="navigation_bar">
+      <nav>
+        <div class="logo">MR KWAZA</div>
+        <ul>
+          <li>
+            <a href="#" class="active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Service</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Navigation;
