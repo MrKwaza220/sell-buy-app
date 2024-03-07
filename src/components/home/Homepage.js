cfb04a8642dcd11e931b80a1d46dbdba7c2c2f3e
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-03-07 09:03:17 +0200

    added user navigation

--- src/components/home/Homepage.js
+++ src/components/home/Homepage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Homepage.css";
 
-function Homepage() {
+const Homepage = () => {
+  const [activeSection, setActiveSection] = useState("home");
   return (
     <section id="home">
       <div className="homepage">
@@ -11,41 +12,62 @@ function Homepage() {
             <h4>Mr Kwaza</h4>
           </div>
 
-          <div class="about_user">
-            <a href="#" class="nav-link" data-section="home">
-              Home
-            </a>
-            <a href="#" class="nav-link" data-section="about">
-              About
-            </a>
-            <a href="#" class="nav-link" data-section="services">
-              Services
-            </a>
-            <a href="#" class="nav-link" data-section="work">
-              Work
-            </a>
+          <div className="about_user">
+            <div className="container">
+              <div className="user_navigation">
+                <a href="#" onClick={() => setActiveSection("home")}>
+                  Home
+                </a>
+                <a href="#" onClick={() => setActiveSection("about")}>
+                  About
+                </a>
+                <a href="#" onClick={() => setActiveSection("services")}>
+                  Services
+                </a>
+                <a href="#" onClick={() => setActiveSection("work")}>
+                  Work
+                </a>
+              </div>
 
-            <div id="home-content" class="section-content">
-              <img src="Renting black.jpg" />
-            </div>
+              
+              <div className="content">
+                {activeSection === "home" && (
+                  <div className="home-content">
+                    <img src="Renting black.jpg" alt="Home" />
+                  </div>
+                )}
 
-            <div id="about-content" class="section-content">
-              <p>About Us</p>
-            </div>
+                {activeSection === "about" && (
+                  <div className="about-content">
+                    {/* Content for About */}
+                    <h1>About Page</h1>
+                    <p>This is the about page content.</p>
+                  </div>
+                )}
 
-            <div id="services-content" class="section-content">
-              <p>Our Services</p>
-            </div>
+                {activeSection === "services" && (
+                  <div className="services-content">
+                    {/* Content for Services */}
+                    <h1>Services Page</h1>
+                    <p>These are our services.</p>
+                  </div>
+                )}
 
-            <div id="work-content" class="section-content">
-              <p>Our Work</p>
+                {activeSection === "work" && (
+                  <div className="work-content">
+                    {/* Content for Work */}
+                    <h1>Work Page</h1>
+                    <p>Our work examples go here.</p>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
+          <p>
+            <button>Message</button>
+          </p>
 
           <div className="user_contacts">
-            <a href="#">
-              <i className="fa fa-dribbble"></i>
-            </a>
             <a href="#">
               <img src="twitter.png" alt="Twitter" />
             </a>
@@ -56,12 +78,9 @@ function Homepage() {
               <img src="Facebook.png" alt="Facebook" />
             </a>
           </div>
-          <p>
-            <button>Message</button>
-          </p>
         </div>
       </div>
     </section>
   );
-}
+};
 export default Homepage;
