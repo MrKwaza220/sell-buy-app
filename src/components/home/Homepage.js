e28d1d4668be27a9b0e20267269a14a51ad6dbbd
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-03-07 08:25:46 +0200

    added contact icons

--- src/components/home/Homepage.js
+++ src/components/home/Homepage.js
@@ -5,31 +5,42 @@ function Homepage() {
   return (
     <section id="home">
       <div className="homepage">
-        <div class="user_card">
-          <img src="userprofile.png" alt="profile" />
-          <h4>Mr Kwaza</h4>
-        </div>
+        <div className="card">
+          <div className="user_card">
+            <img src="userprofile.png" alt="profile" />
+            <h4>Mr Kwaza</h4>
+          </div>
 
-        <div class="about_user">
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">Services</a>
-          <a href="">Work</a>
+          <div className="about_user">
+            <a href="">
+              Home
+              <div className="user_home">
+               
+              </div>
+            </a>
+            <a href="">About</a>
+            <a href="">Services</a>
+            <a href="">Work</a>
+          </div>
+
+          <div className="user_contacts">
+            <a href="#">
+              <i className="fa fa-dribbble"></i>
+            </a>
+            <a href="#">
+              <img src="twitter.png" alt="Twitter" />
+            </a>
+            <a href="#">
+            <img src="linkedln.png" alt="Linkedln" />
+            </a>
+            <a href="#">
+            <img src="Facebook.png" alt="Facebook" />
+            </a>
+          </div>
+          <p>
+            <button>Message</button>
+          </p>
         </div>
-        <div>
-          <a href="#">
-            <img src="twitter.png" alt="x" />
-          </a>
-          <a href="#">
-            <img src="facebook.png" alt="facebook" />
-          </a>
-          <a href="#">
-            <img src="linkedln.png" alt="linkedln" />
-          </a>
-        </div>
-        <p>
-          <button>Contact</button>
-        </p>
       </div>
     </section>
   );
