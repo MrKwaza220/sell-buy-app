6070e41ae7eadd449aa436a0bc8040b9998cbfb7
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-03-12 12:08:06 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Homepage from './components/home/Homepage';
 import About from './components/about/About';
+import Message from './components/messages/Messages';
+import Notifications from './components/notifications/Notifications';
+import Favourites from './components/favourites/Favourite';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Homepage />
+      <Message/>
+      <Notifications/>
+      <Favourites/>
       <About />
     </div>
   );
