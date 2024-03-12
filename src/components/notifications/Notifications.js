5920c044490b50e076e0bbf58095017e1ea6efd1
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-03-12 12:01:01 +0200

    added nofications

---
+++ src/components/notifications/Notifications.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Notifications.css';
+
+const Notifications = () => {
+    return (
+      <section id="notifications">
+        <div>Notification</div>
+      </section>
+    );
+  };
+  export default Notifications;
