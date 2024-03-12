d332b90bdedceb8bb62a534abd123f38b154f093
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-03-12 11:58:01 +0200

    added favourites

---
+++ src/components/favourites/Favourite.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Favourites.css';
+
+const Favourites = () =>
+{
+    return(
+        <section>
+            <div>
+                Favourites
+            </div>
+        </section>
+    )
+}
+export default Favourites;
