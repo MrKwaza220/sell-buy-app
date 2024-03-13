9dafa1d3400facbab34f5062e07b28e5160d12c2
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-03-13 10:43:19 +0200

    added username chats

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,13 +1,27 @@
 import React from "react";
-import './Messages.css';
+import "./Messages.css";
 
 const Message = () => {
-    return (
-      <section id="userMessages">
-        <h2>Messages</h2>
-
-
-      </section>
-    );
-  };
-  export default Message ;
+  return (
+    <section id="userMessages">
+      <h2>Messages</h2>
+      <div className="usersChats">
+        <div className="search_chats">
+          <input type="text" className="search-input" placeholder="Search" />
+          <div className="search-icon">
+            <img src="search.png" alt="Search" />
+          </div>
+          <div className="users_message">
+            <div className="user_chat">
+              <h3>Username</h3>
+              <div className="activity">
+                Typing
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Message;
