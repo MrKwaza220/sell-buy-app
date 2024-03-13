122759159c5cccea66d26db01f3034c597267ee4
MrKwaza220 <sakhumzikwaza220@gmail.com>
2024-03-13 13:12:07 +0200

    added userchat to seperate class

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -7,20 +7,43 @@ const Message = () => {
       <h2>Messages</h2>
       <div className="usersChats">
         <div className="search_chats">
+
           <input type="text" className="search-input" placeholder="Search" />
           <div className="search-icon">
             <img src="search.png" alt="Search" />
           </div>
+        </div>
+
           <div className="users_message">
             <div className="user_chat">
               <h3>Username</h3>
               <div className="activity">
-                Typing
+                <p>Typing...</p>
+              </div>
+            </div>
+
+            <div className="user_chat">
+              <h3>Username</h3>
+              <div className="activity">
+              <p>Typing...</p>
+              </div>
+            </div>
+
+            <div className="user_chat">
+              <h3>Username</h3>
+              <div className="activity">
+              <p>Typing...</p>
+              </div>
+            </div>
+
+            <div className="user_chat">
+              <h3>Username</h3>
+              <div className="activity">
+              <p>Typing...</p>
               </div>
             </div>
           </div>
         </div>
-      </div>
     </section>
   );
 };
